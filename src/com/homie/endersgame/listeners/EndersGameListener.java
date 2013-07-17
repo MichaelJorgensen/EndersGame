d23ec2a3d233fb4fd3b54ac9a74b1061cd9177f5
Mike <dontlookbehindyou@live.com>
2013-07-17 10:44:32 -0700

    Player's armor contents removed from storage now when they leave

--- src/com/homie/endersgame/listeners/EndersGameListener.java
+++ src/com/homie/endersgame/listeners/EndersGameListener.java
@@ -90,7 +90,7 @@ public class EndersGameListener implements Listener {
 				HashMap<String, GameTeam> players = gm.getGamePlayers(i);
 				if (players.containsKey(player.getName())) {
 					GameListener.players_hit.remove(player.getName());
-					if (GameListener.times_players_hit.containsKey(player.getName())) GameListener.times_players_hit.remove(player.getName());
+					GameListener.times_players_hit.remove(player.getName());
 					player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
 					player.teleport(player.getLocation().getWorld().getSpawnLocation());
 					player.setFlySpeed(0.1f);
@@ -102,6 +102,7 @@ public class EndersGameListener implements Listener {
 					player.updateInventory();
 					EndersGame.playing_players_gamemode.remove(player.getName());
 					EndersGame.playing_players_inventory.remove(player.getName());
+					EndersGame.player_players_armor.remove(player.getName());
 					players.remove(player.getName());
 					gm.updateGamePlayers(i, players);
 					EventHandle.callPlayerLeaveEndersGameEvent(i, player);
